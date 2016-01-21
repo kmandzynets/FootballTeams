5dccd8b6179b414cd20af6779901c9d8a3a823ec
yaroslavkulak <yaroslavkulak@gmail.com>
2016-01-21 12:44:07 +0200

    Update TeamsParser.java

--- FootballTeams/src/main/java/com/lits/FootballTeams/TeamsParser.java
+++ FootballTeams/src/main/java/com/lits/FootballTeams/TeamsParser.java
@@ -5,7 +5,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Collections;
-
 import java.util.List;
 
 import com.fasterxml.jackson.core.JsonParseException;
